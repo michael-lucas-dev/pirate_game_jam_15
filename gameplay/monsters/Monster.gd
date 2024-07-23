120150fea65c5f474eabe0c310ef41a447203ab9
Michael Lucas <pro@michaellucas.fr>
2024-07-23 05:19:45 +0200

    Create 4 different enemies

--- gameplay/monsters/Monster.gd
+++ gameplay/monsters/Monster.gd
@@ -1,6 +1,17 @@
 class_name Monster
 extends Area2D
 
-@export var Element : String
-@export var speed : float = 1
+enum Element {AIR, WATER, FIRE, EARTH}
 
+@export var element : Element
+@export var speed : float = 1
+@export var health : float = 10
+
+func _attack():
+	print("To implement attack")
+	
+func _defend():
+	print("To implement defense")
+	
+func _move():
+	print("To implement move")
